96a3094642d8aa2a5df28c0a3f63b5e6a9f3bd22
ubeyid oumer <ubeyidah@gmail.com>
2024-05-01 22:15:20 +0300

    subscribe and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,63 @@
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-container">
+      <div className="flex justify-between items-center gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="logo" width={150} height={46} />
+          </a>
+          <p className="capitalize text-base leading-2  mt-3 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            your perfect size in store. Get Rewards
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => {
+              return (
+                <div
+                  key={icon.alt}
+                  className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+                >
+                  <img src={icon.src} alt={icon.alt} width={24} height={24} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="flex flex-1 justify-between xl:gap-20 gap-10 flex-wrap">
+          {footerLinks.map((section) => {
+            return (
+              <div key={section.title}>
+                <h4 className="text-white font-montserrat text-2xl leading-normal mb-6">
+                  {section.title}
+                </h4>
+                <ul>
+                  {section.links.map((link) => (
+                    <li
+                      key={link.name}
+                      className="mt-3 text-white-400 font-montserrat hover:underline"
+                    >
+                      <a href="#">{link.name}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="flex justify-between text-white-400 mt-20 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 cursor-pointer">
+          &copy; <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,5 +1,25 @@
+import Button from "./Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-center items-center  gap-10 flex-col"
+      id="contact-us"
+    >
+      <h3 className="text-4xl font-semibold leading-[68px]  font-palanquin md:text-nowrap">
+        Sign up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+      <div className="flex flex-1 border border-slate-gray rounded-full p-1 pl-3 w-full  max-w-lg">
+        <input
+          type="text"
+          placeholder="subscribe@nike.com"
+          className="input flex-1 border-none"
+        />
+        <Button label="Subscribe " />
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
