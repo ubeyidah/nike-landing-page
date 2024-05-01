ff03dbec3717aadf84ce50649a1d4259e5499493
ubeyid oumer <ubeyidah@gmail.com>
2024-05-01 17:20:38 +0300

    create hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
 const Button = (props) => {
   return (
-    <button className="flex items-center gap-3 py-2 px-5 rounded-full bg-coral-red text-white cursor-pointer hover:opacity-80 active:opacity-100">
-      {props.label} <img src={props.iconURL || ""} alt="arrow" />
+    <button className="flex items-center gap-3 py-2 px-5 rounded-full bg-coral-red text-white cursor-pointer hover:opacity-80 active:opacity-100 font-montserrat text-sm transition-opacity duration-200">
+      {props.label}{" "}
+      <img src={props.iconURL || ""} width={20} height={20} alt="arrow" />
     </button>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,72 @@
 import Button from "./Button";
 import { arrowRight } from "../assets/icons";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "./ShoeCard";
+import { useState } from "react";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section
       id="home"
-      className="w-full p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="xl:w-2/5 flex flex-col relative justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collections</p>
-        <h1>
-          <span>The New Arrival</span>
+      <div className="xl:w-2/5 flex flex-col relative justify-center items-start w-full max-xl:padding-x pt-28 ">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collections
+        </p>
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike </span>
+          <span className="text-coral-red inline-block mt-3">Nike </span>
           Shoes
         </h1>
-        <p>
+
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
+
         <Button label="Shop now" iconURL={arrowRight} />
+
+        <div className="flex items-start justify-start flex-wrap w-full mt-20 gap-12 ">
+          {statistics.map((item) => (
+            <div key={item.label}>
+              <p className="text-3xl font-palanquin font-bold mb-0">
+                {item.value}
+              </p>
+              <p className="mt-0 text-slate-gray font-montserrat">
+                {item.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoeImg}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[6%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeShoesImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
